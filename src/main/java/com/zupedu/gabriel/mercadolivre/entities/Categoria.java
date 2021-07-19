326f4f364254539b37bf06ec502c9a3fa3175b9b
Gabriel <gabrielcpcontato@outlook.com>
2021-07-19 17:42:47 -0300

    Criando classe Categoria e camada de acesso ao banco

---
+++ src/main/java/com/zupedu/gabriel/mercadolivre/entities/Categoria.java
@@ -0,0 +1,13 @@
+package com.zupedu.gabriel.mercadolivre.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/zupedu/gabriel/mercadolivre/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.zupedu.gabriel.mercadolivre.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.zupedu.gabriel.mercadolivre.entities.Categoria;
+import com.zupedu.gabriel.mercadolivre.entities.Usuario;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
